13282195ab1c14b79a99e5714eacee98f42161ac
Ömer Kılıç <123635257+omerkilic-0@users.noreply.github.com>
2023-08-14 12:53:10 +0300

    Add files via upload

---
+++ F1/index.php
@@ -0,0 +1,4 @@
+<?php include 'round/round.php'; ?>
+<?php include 'round/header.php'; ?>
+<?php include 'round/content.php'; ?>
+<?php include 'round/footer.php'; ?>
---
+++ F1/pitTime.php
@@ -0,0 +1,4 @@
+<?php include 'pitTime/pitTime.php'; ?>
+<?php include 'pitTime/header.php'; ?>
+<?php include 'pitTime/content.php'; ?>
+<?php include 'pitTime/footer.php'; ?>
