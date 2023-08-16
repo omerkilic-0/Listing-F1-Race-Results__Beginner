d76f98efe301aea3d59b19ef1d5578cf8d25183a
Ömer Kılıç <123635257+omerkilic-0@users.noreply.github.com>
2023-08-16 10:16:45 +0300

    Add files via upload
    Project files have been converted to MVC format and Guess page has been added as the 3rd page. On this page, the top 5 of the actual race are estimated based on the qualifying laps via the ergast.com API and Pit Stop Duration times are added.

---
+++ F1/control/round.php
@@ -0,0 +1,22 @@
+<?php
+
+//API gelen verilere varsayılan değer atıyoruz.
+$raceName = $circuitName = $date = $position = $givenName = $familyName = $name = $grid = $laps = $status = $lap = $time = $speed = $result = $yearInput = $roundInput = $resultCount = "";
+
+//Butona basındı ise.
+if (isset($_POST["list"])) {
+
+    $yearInput = $_POST['year'];
+    $roundInput = $_POST['round'];
+
+    include 'model/round.php';
+
+    curl_close($ch);
+
+    if (isset($response->MRData->RaceTable->Races[0]->Results)) {
+        $result = $response->MRData->RaceTable->Races[0]->Results;
+        $resultCount = count($result);
+    } else {
+        echo '<div class="alert alert-danger" role="alert"><center>Wrong round.</center></div>';
+    }
+}
---
+++ F1/guess.php
@@ -0,0 +1,4 @@
+<?php include 'control/guess.php'; ?>
+<?php include 'view/header/guess.php'; ?>
+<?php include 'view/content/guess.php'; ?>
+<?php include 'view/footer/guess.php'; ?>
