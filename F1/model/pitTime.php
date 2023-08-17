16a88164f97e7e29ac2fee99d76a2523f00c9ad0
Ömer Kılıç <123635257+omerkilic-0@users.noreply.github.com>
2023-08-17 12:45:24 +0300

    Delete F1/model directory
    Hatalı yükleme olduğu için silindi.

--- F1/model/pitTime.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$url = "http://ergast.com/api/f1/" . $yearInput . "/" . $roundInput . "/pitstops/" . $pitInput . ".json";
-
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($ch);
-        $response = json_decode($response);
--- F1/model/round.php
+++
@@ -1,9 +0,0 @@
-<?php
-$url = "https://ergast.com/api/f1/" . $yearInput . "/" . $roundInput . "/results.json";
-
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec($ch);
-$response = json_decode($response);
-
-
