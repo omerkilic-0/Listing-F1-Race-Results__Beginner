0a3b06ce6789fefe149e18c4c6bf48a02b9315eb
Ömer Kılıç <123635257+omerkilic-0@users.noreply.github.com>
2023-08-17 12:46:33 +0300

    Delete pilot_details.php
    Hatalı yükleme olduğu için silindi.

--- F1/pilot_details.php
+++
@@ -1,4 +0,0 @@
-<?php include 'control/pilot_details.php'; ?>
-<?php include 'view/header/pilot_details.php'; ?>
-<?php include 'view/content/pilot_details.php'; ?>
-<?php include 'view/footer/pilot_details.php'; ?>
