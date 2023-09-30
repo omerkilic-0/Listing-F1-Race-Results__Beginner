d370eec111093b69f5573840f14207593cd8a6de
Ömer Kılıç <komer4542@gmail.com>
2023-09-30 23:11:51 +0300

    #1 folders updated

---
+++ F1/views/partials/footer.php
@@ -0,0 +1,11 @@
+<div class="card-footer text-body-secondary">
+    <center>
+        <p>Design by <b>ÖMER KILIÇ </b> | &copy; 2023</p>
+    </center>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
---
+++ F1/views/partials/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+    <link rel="shortcut icon" href="../img/f1.png" type="image/x-icon">
+    <link rel="stylesheet" href="../style/style.css">
+    <title>F1</title>
+</head>
